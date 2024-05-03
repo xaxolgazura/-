2e292e925bbb2e6f062a46cd68baa4bce0f3137b
иван <xaxolgazarov@mail.ru>
2024-05-03 16:37:03 +0300

    commit message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int x = 4;
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println(fullName.toUpperCase());
+
+
+        String fullname = "Иванов Семён Семёнович";
+        String fullnameTrue = fullname.replace("ё", "е");
+        System.out.println(fullnameTrue);
     }
 }
